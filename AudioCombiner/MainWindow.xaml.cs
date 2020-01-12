bb48bd4ddd9221e77bef2020fc167c419a4657b0
Kodai Takao <kodai100@gmail.com>
2020-01-13 00:39:24 +0900

    Initial Commit

---
+++ AudioCombiner/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace AudioCombiner
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void OnClick1(object sender, RoutedEventArgs e)
+        {
+            
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.Filter = "Audio File(.mp3)|*.mp3|Audio File(*.wav)|*.wav";
+            bool? result = openFileDialog.ShowDialog();
+            if (result == true)
+            {
+                text1.Text = openFileDialog.FileName;
+            }
+        }
+
+        void OnClick2(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.Filter = "Audio File(.mp3)|*.mp3|Audio File(*.wav)|*.wav";
+            bool? result = openFileDialog.ShowDialog();
+            if (result == true)
+            {
+                text2.Text = openFileDialog.FileName;
+            }
+        }
+
+        void OnClick3(object sender, RoutedEventArgs e)
+        {
+
+            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+
+                text3.Text = dialog.SelectedPath;
+            }
+
+        }
+
+
+        private void DropList_Drop(object sender, DragEventArgs e)
+        {
+
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                // Note that you can have more than one file.
+                string[] files = (string[]) e.Data.GetData(DataFormats.FileDrop);
+
+                // Assuming you have one file that you care about, pass it off to whatever
+                // handling code you have defined.
+
+                foreach (var file in files)
+                {
+                    lb.Items.Add(file);
+                }
+               
+                
+            }
+        }
+    }
+}
